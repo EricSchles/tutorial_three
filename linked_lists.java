4316476abde5eb99252b057a29eccb185f094de0
EricSchles <ericschles@gmail.com>
2015-01-05 15:25:54 -0500

    started linked list finished node

---
+++ linked_lists.java
@@ -0,0 +1,27 @@
+class Node<T>{
+	public T cargo;
+	public Node next;
+
+	public Node(){
+		this.cargo = 0;
+		this.next = null;
+	}
+	public Node(cargo,next){
+		this.cargo = cargo;
+		this.next = next;
+	}
+
+	public String toString(){
+		return cargo + "";
+	}
+}
+
+class linked_list{
+	Node<Integer> head;
+
+	
+}
+class linked_lists{
+
+
+}
